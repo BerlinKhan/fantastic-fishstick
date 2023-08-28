6630d459b9113b340eba583b141b03ef2a65939e
kashif Ahmad <kashifahmad124590@gmail.com>
2023-08-28 23:02:08 +0500

    Commit after major changes

--- Readme.md
+++ Readme.md
@@ -1,2 +1,9 @@
 #Cowlar Tasks
 This is a read me File for the task assigned to me by cowlar design studio.
+
+There are three tasks in this repository in the folder cypress/e2e/
+
+#To Run The Task
+1. Make sure that you have already run "npm init" or have a node_modules folder or package.json file in the root of your project to ensure cypress is installed in the correct directory.
+2. install cypress using npm install cypress --save-dev
+3. You can open Cypress from your project root by using "npx cypress open"
--- cypress/e2e/Task1.cy.js
+++ cypress/e2e/Task1.cy.js
@@ -1,58 +1,60 @@
 describe('Cypress Form Test', () => {
   it('Fills out the form and verifies modal information', () => {
+   
     // Changing the viewport as the website was not responsive on default view.
     cy.viewport(2048,1152)
     
-    // Visit the specified URL
+    // Visit the URL
     cy.visit('https://demoqa.com/');
+    // waiting 2 seconds to confirm the site loaded properly
     cy.wait(2000);
     
 
-    // Click on the Forms Button
-    cy.contains('Forms').scrollIntoView().click();
+    // Clicking on the forms Buttom
+    cy.contains('Forms').click();
+    // waiting 2 seconds
     cy.wait(2000);
 
-    // Click on Practice Form button from the left sidebar
+    // Clicking on Practice Form button from the left sidebar
     cy.contains('Practice Form').click();
 
-    // Enter the form data
+    // Entering the data in the form
     cy.get('#firstName').type('Cowlar');
     cy.get('#lastName').type('Developer');
     cy.get('#userEmail').type('qaengineer@cowlar.com');
 
-    // Click on the Gender radio button using the label
+    // Clicking on radio button using the label 
     cy.get('label[for="gender-radio-1"]').click();
 
+    //Entering the mentioned number
     cy.get('#userNumber').type('0123456789');
 
-    // Enter Subjects
-    // Target the input element inside the complex structure
+    // Entering and selecting "Computer Science" Subject
     cy.get('.subjects-auto-complete__input input').click().type('Computer Science{Enter}');
 
-    // Click on Hobbies checkbox using the label
+    // Clicking on checkbox "Music"
     cy.get('label[for="hobbies-checkbox-3"]').click();
 
+    //typing adress
     cy.get('#currentAddress').type('Address 1');
-   // Select the desired state from the dropdown
-    // Click to open the dropdown
+  
 
-
-
-     // Select State
+     // Entering and selecting State"NCR"
      cy.get('#state').click().type('NCR{enter}');
 
-     // Select City
+     // Entering and selecting City "Delhi"
      cy.get('#city').click().type('Delhi{enter}');
 
     // Click on the Submit button
     cy.get('#submit').click();
 
+    //waiting for 3 seconds
     cy.wait(3000);
 
-    // Verify modal information
-    cy.get('.modal-content').should('contain', 'Cowlar');
-    cy.get('.modal-content').should('contain', 'Developer');
-    cy.get('.modal-content').should('contain', 'qaengineer@cowlar.com');
+    // Verifying that the information presented in the Modal is the same as provided
+    cy.get('.modal-content').should('contain', 'Cowlar'); // Verify First Name
+    cy.get('.modal-content').should('contain', 'Developer'); // Verify Last Namae
+    cy.get('.modal-content').should('contain', 'qaengineer@cowlar.com');// Verify Email
     cy.get('.modal-content').should('contain', 'Male'); // Verify Gender
     cy.get('.modal-content').should('contain', '0123456789'); // Verify Mobile
     cy.get('.modal-content').should('contain', 'Computer Science'); // Verify Subjects
@@ -61,7 +63,7 @@ describe('Cypress Form Test', () => {
     cy.get('.modal-content').should('contain', 'NCR'); // Verify State
     cy.get('.modal-content').should('contain', 'Delhi'); // Verify City
 
-    // Close the Modal
+    // Closing the Modal
     cy.get('#closeLargeModal').click();
   });
 });
